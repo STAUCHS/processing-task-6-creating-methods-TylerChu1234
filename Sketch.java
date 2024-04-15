46798b902e272d12f7f19e544050e1ebc8adb3f0
TylerChu1234 <tylerchu.tyc@gmail.com>
2024-04-15 15:42:28 -0400

    asdasdasdasd

--- Sketch.java
+++ Sketch.java
@@ -2,14 +2,6 @@ import processing.core.PApplet;
 import java.util.Random;
 
 public class Sketch extends PApplet {
-	
-
-  
-
-
-
-
-
 
   public void settings() {
     size(400, 400);
@@ -21,31 +13,36 @@ public class Sketch extends PApplet {
 
   public void draw() {
 
-  Random myColor = new Random();
+    //Set variable for random
+    Random myColor = new Random();
 
-  int intXFlower = 0;
-  int intYFlower = 0;
-    for (int intRow = 5; intRow < 50; intRow+=10) {
-      for (int intColumn = 5; intColumn < 50; intColumn+=10) {
-        intXFlower =  intRow * 10;  
-        intYFlower =  intColumn * 10; 
-        drawFlower(intXFlower,intYFlower,10,myColor.nextInt(256),myColor.nextInt(256),myColor.nextInt(256));
+    
+    int intXFlower = 0;
+    int intYFlower = 0;
+      for (int intRow = 5; intRow < 50; intRow+=10) {
+        for (int intColumn = 5; intColumn < 50; intColumn+=10) {
+          intXFlower =  intRow * 10;  
+          intYFlower =  intColumn * 10; 
+          drawFlower(intXFlower,intYFlower,10,myColor.nextInt(256),myColor.nextInt(256),myColor.nextInt(256));
+        }
       }
-    }
-  int intXFace = 0;
-  int intYFace = 0;
-    for (int intRow = 5; intRow < 100; intRow+=20) {
-      for (int intColumn = 5; intColumn < 100; intColumn+=20) {
-        intXFace =  intRow * 5;  
-        intYFace =  intColumn * 5; 
-        drawFace(intXFace,intYFace,30,10,10,10);
-      } 
-    }
+
+    int intXFace = 0;
+    int intYFace = 0;
+      for (int intRow = 5; intRow < 100; intRow+=20) {
+        for (int intColumn = 5; intColumn < 100; intColumn+=20) {
+          intXFace =  intRow * 5;  
+          intYFace =  intColumn * 5; 
+          drawFace(intXFace,intYFace,30,10,10,10);
+        } 
+      }
+
   }
   
 
   public void drawFlower(float flowerX, float flowerY, float petalSize, float myColor, float myColor2, float myColor3) {
 
+    // Set distance of petal from the center
     float petalDistance = petalSize / 2;
     
     // Set color for petals
@@ -66,41 +63,46 @@ public class Sketch extends PApplet {
     // center petal
     ellipse(flowerX, flowerY, petalSize, petalSize);
 
-   
   }
   
-
   public void drawFace(float faceX, float faceY, float faceSize, float LeftEye, float RightEye, float Mouth) {
 
+    // Reset the color to white
     fill (255,255,255);
 
+    // Head
     ellipse (faceX,faceY,faceSize,faceSize);
 
+    // Set left eye color to black
     fill(0,0,0);
 
+    // Left-eye
     ellipse (faceX-width/50, faceY-width/60,faceSize/5,faceSize/5); 
 
+    // Set right eye color to black
     fill(0,0,0);
 
+    // Right-eye
     ellipse (faceX+width/50, faceY-width/60,faceSize/5,faceSize/5);
 
+    // Set mouth color to red
     fill(255,5,5);
 
+    // Mouth
     ellipse (faceX-width/500, faceY-width/4,faceSize/2,faceSize/5);
 
-    fill(255,255,255);
-
   }
+}
+
   
   /**
-   * Description
+   * A program that uses methods and parameters to draw an inanimate object and an object that can change color
    * 
+   * @param intAnountEntities
    * @param 
-   * @param 
-   * @return
-   * @author 
+   * @return 
+   * @author T. Chu
    */
 
   
   
-}
